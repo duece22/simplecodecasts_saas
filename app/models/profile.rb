b72d15caf22dd4d57977f05df1adc46009e798f5
Devon Jones <devonjones510@gmail.com>
2015-08-23 04:04:21 +0000

    Added profiles form and create action

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,5 @@
 class Profile < ActiveRecord::Base
     belongs_to :user
-    
+    validates :first_name, :last_name, :email, :phone_number, presence: true
+    validates :phone_number, length: {is: 7}
 end
