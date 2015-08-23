48c192fa09a89ebdeaf05fd53394018ee17de253
Devon Jones <devonjones510@gmail.com>
2015-08-23 05:36:04 +0000

    Added bare bones user show page

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,4 @@
 class Profile < ActiveRecord::Base
     belongs_to :user
-    #validates :first_name, :last_name, :contact_email, :phone_number, presence: true
-    #validates :phone_number, length: {is: 7}
+    validates :first_name, :last_name, :contact_email, presence: true
 end
